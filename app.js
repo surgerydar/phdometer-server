b6cc49983393401d5ded5346d4f93d0382a1cd53
jons <jons@surgerydar.co.uk>
2017-09-30 16:16:16 +0100

    debugging app startup

--- app.js
+++ app.js
@@ -306,14 +306,15 @@ db.connect(
         }
     });
     
+    //
+    // start app
+    //
+    console.log( 'starting application : port : ' + env.PORT || env.OPENSHIFT_NODEJS_PORT || 8080 + " : ip : " env.IP || env.OPENSHIFT_NODEJS_IP || '0.0.0.0' );
+    app.listen( env.PORT || env.OPENSHIFT_NODEJS_PORT || 8080, env.IP || env.OPENSHIFT_NODEJS_IP || '0.0.0.0', function () {
+      console.log('Application worker ' + process.pid + ' started...');
+    });
+    
 
 }).catch( function( err ) {
 	console.log( 'unable to connect to database : ' + err );
-});
-//
-// start app
-//
-console.log( 'starting application' );
-app.listen( env.PORT || env.OPENSHIFT_NODEJS_PORT || 8080, env.IP || env.OPENSHIFT_NODEJS_IP || '0.0.0.0', function () {
-  console.log('Application worker ' + process.pid + ' started...');
 });
